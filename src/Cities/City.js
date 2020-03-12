5aab0a6689bfdf8341296c500f21b5435c0109ee
yoonseongd <dys0716@gmail.com>
2020-03-12 18:25:38 +0900

    []

---
+++ src/Cities/City.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableWithoutFeedback,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+
+import CenterMessage from "..//components/CenterMessage";
+
+import { colors } from "../theme";
+
+class City extends React.Component {}
