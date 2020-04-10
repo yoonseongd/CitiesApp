96fa326a7798ea83de1fd48ed8f0010bbc9c48b7
yoonseongd <dys0716@gmail.com>
2020-04-10 18:15:02 +0900

    [스크롤 뷰의 아래 두 세개 아이템이 스크롤 뷰 아래있는 컴포넌트 뒤로 가려지고, 텍스트 인풋 눌러서 키보드가 화면에 나올때 맨 밑에 렌더링 되는 컴포넌트가 키보드 뒤로 가려지지 않게 하는 수정사항]

--- src/Cities/City.js
+++ src/Cities/City.js
@@ -6,7 +6,7 @@ import {
   ScrollView,
   TouchableWithoutFeedback,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
 } from "react-native";
 
 import CenterMessage from "..//components/CenterMessage";
@@ -14,24 +14,24 @@ import CenterMessage from "..//components/CenterMessage";
 import { colors } from "../theme";
 
 class City extends React.Component {
-  static navigationOptions = props => {
+  static navigationOptions = (props) => {
     const { city } = props.navigation.state.params;
     return {
       title: city.city,
       headerTitleStyle: {
         color: "white",
         fontSize: 20,
-        fontWeight: "400"
-      }
+        fontWeight: "400",
+      },
     };
   };
   state = {
     name: "",
-    info: ""
+    info: "",
   };
   onChangeText = (key, value) => {
     this.setState({
-      [key]: value
+      [key]: value,
     });
   };
 
@@ -40,7 +40,7 @@ class City extends React.Component {
     const { city } = this.props.navigation.state.params;
     const location = {
       name: this.state.name,
-      info: this.state.info
+      info: this.state.info,
     };
     this.props.screenProps.addLocation(location, city);
     this.setState({ name: "", info: "" });
@@ -50,46 +50,54 @@ class City extends React.Component {
     const { city } = this.props.navigation.state.params;
     return (
       <View style={{ flex: 1 }}>
-        <ScrollView
-          contentContainerStyle={[!city.locations.length && { flex: 1 }]}
-        >
-          <View
-            style={[
-              styles.locationContainer,
-              !city.locations.length && { flex: 1, justifyContent: "center" }
-            ]}
+        <View style={{ flex: 1 }}>
+          <ScrollView
+            style={styles.scrollViewStyle}
+            contentContainerStyle={[!city.locations.length && { flex: 1 }]}
           >
-            {!city.locations.length && (
-              <CenterMessage message="No locations for this city!" />
-            )}
-            {city.locations.map((location, index) => (
-              <View key={index} style={styles.locationContainer}>
-                <Text style={styles.locationName}>{location.name}</Text>
-                <Text style={styles.locationInfo}>{location.info}</Text>
-              </View>
-            ))}
-          </View>
-        </ScrollView>
-        <TextInput
-          onChangeText={val => this.onChangeText("name", val)}
-          placeholder="Location name"
-          value={this.state.name}
-          style={styles.input}
-          placeholderTextColor="white"
-        />
-        <TextInput
-          onChangeText={val => this.onChangeText("info", val)}
-          placeholder="Location info"
-          value={this.state.info}
-          style={[styles.input, styles.input2]}
-          placeholderTextColor="white"
-        />
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={this.addLocation}>
-            <View style={styles.button}>
-              <Text style={styles.buttonText}>Add Location</Text>
+            <View
+              style={[
+                { padding: 10, flex: 1 },
+                !city.locations.length && styles.locationContainer,
+                !city.locations.length && { justifyContent: "center" },
+              ]}
+            >
+              {!city.locations.length && (
+                <CenterMessage message="No locations for this city!" />
+              )}
+              {city.locations.map((location, index) => (
+                <View key={index} style={styles.locationContainer}>
+                  <Text style={styles.locationName}>{location.name}</Text>
+                  <Text style={styles.locationInfo}>{location.info}</Text>
+                </View>
+              ))}
             </View>
-          </TouchableOpacity>
+          </ScrollView>
+        </View>
+
+        <View>
+          <TextInput
+            onChangeText={(val) => this.onChangeText("name", val)}
+            placeholder="Location name"
+            value={this.state.name}
+            style={styles.input}
+            placeholderTextColor="white"
+          />
+          <TextInput
+            onChangeText={(val) => this.onChangeText("info", val)}
+            placeholder="Location info"
+            value={this.state.info}
+            style={[styles.input, styles.input2]}
+            placeholderTextColor="white"
+          />
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={this.addLocation}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Add Location</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     );
@@ -98,10 +106,12 @@ class City extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
+  scrollViewStyle: { width: "100%", position: "absolute", height: 400 },
+
   locationsContainer: {
-    paddingBottom: 104
+    paddingBottom: 104,
   },
   input: {
     height: 50,
@@ -111,31 +121,58 @@ const styles = StyleSheet.create({
     position: "absolute",
     width: "100%",
     bottom: 104,
-    left: 0
+    left: 0,
   },
   input2: {
-    bottom: 52
+    bottom: 52,
   },
   buttonContainer: {
     height: 50,
     backgroundColor: colors.primary,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   buttonText: {
-    color: "white"
+    color: "white",
   },
   locationContainer: {
     padding: 10,
     borderBottomColor: colors.primary,
-    borderBottomWidth: 2
+    borderBottomWidth: 2,
   },
   locationName: {
-    fontSize: 20
+    fontSize: 20,
   },
   locationInfo: {
-    color: "rgba(0,0,0,.5)"
-  }
+    color: "rgba(0,0,0,.5)",
+  },
 });
 
 export default City;
+
+//수정사항
+// -(1. 문제 : android의 scroll view가 모바일 화면 전체를 갖는다.
+// 물론 location name, info, add location 텍스트인풋과 버튼은 스크롤 뷰 위에서 보여 가려지진 않지만
+// 스크롤뷰의 맨 밑에있는 2,3개의 아이템이 텍스트인풋과 버튼 뒤에 가려져서 보이지 않는다.
+// 스크롤뷰와 텍스트인풋 두 사이가 곂치게 않게하려고한다. )
+
+//  2. 해결방법
+// <ScrollView
+// contentContainerStyle={[!city.locations.length && { flex: 1 }]}
+// >
+//   이 부분에서
+
+//   --style={styles.scrollViewStyle}을 ScrollView에 넣어서 수정하엿다. 즉
+//   <ScrollView
+//       style={styles.scrollViewStyle}
+//       contentContainerStyle={[!city.locations.length && { flex: 1 }]}
+//     >
+//       가 되었음.
+//       물론 스타일은  position: "absolute", height: 400 주었다.
+
+//   --onst styles = StyleSheet.create({
+//   scrollViewStyle: { width: "100%", position: "absolute", height: 400 }
+//   })
+
+//만약 두개의 텍스트 인풋을  position: "absolute" 이녀석의 포지션을 상댓값으로하면
+// 텍스트 인풋을 눌러서 키보드 창이 밑에서 시작되어 화면에 올라왔을때 add location을 가려버리게 된다.
